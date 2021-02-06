180d2fbf875fd91c9eb30723748d3dd0dabf7a32
scRaul <61263078+scRaul@users.noreply.github.com>
2021-02-05 19:10:05 -0800

    Paddles Working

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -6,11 +6,13 @@ public class InputManager : MonoBehaviour
 {
     [SerializeField] Paddle leftPaddle;
     [SerializeField] Paddle rightPaddle;
-    public Vector3 movementInput { get; private set; }
+    public Vector3 movementLeftInput { get; private set; }
+    public Vector3 movementRightInput { get; private set; }
     private void Update()
     {
-        movementInput = new Vector3(0, Input.GetAxis("Vertical"), 0);
-        if(Input.GetKeyDown(KeyCode.W))
-            leftPaddle.Move();
+        movementLeftInput = new Vector3(0, Input.GetAxis("Left"), 0);
+        movementRightInput = new Vector3(0, Input.GetAxis("Right"), 0);
+        leftPaddle.Move(movementLeftInput);
+        rightPaddle.Move(movementRightInput);
     }
 }
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -2,22 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(InputManager))]
 public class Paddle : MonoBehaviour
 { 
     public float speed = 1;
-
-    private InputManager playerInput;
-
-    private void Awake()
+  
+    public void Move(Vector3 movement)
     {
-        playerInput = GetComponent< InputManager>();
-    }
-    public void Move()
-    {
-        Vector3 movement = playerInput.movementInput;
         movement *= speed * Time.deltaTime;
         transform.Translate(0f, movement.y, 0f);
     }
+  
 
 }

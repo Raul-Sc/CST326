b76af26d2ab4818fc54b550622965adbcd2750a0
scRaul <61263078+scRaul@users.noreply.github.com>
2021-02-05 20:24:49 -0800

    limit paddle pos

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 { 
-    public float speed = 1;
+    private float speed = 20;
   
     public void Move(Vector3 movement)
     {
+
         movement *= speed * Time.deltaTime;
-        transform.Translate(0f, movement.y, 0f);
+        //limit paddle - y position
+        if (movement.y < 0)
+            if (transform.position.y > -25f)
+                transform.Translate(0f, movement.y, 0f);
+        //limit paddle + y position
+        if(movement.y > 0)
+            if (transform.position.y < 25f)
+                transform.Translate(0f, movement.y, 0f);
+
+
     }
-  
 
 }

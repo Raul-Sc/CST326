b1f92ae77e9e9a21f7d8050b44ed511d9c4ca521
scRaul <61263078+scRaul@users.noreply.github.com>
2021-02-21 17:45:24 -0800

    Part 1

--- SuperMario/Assets/Scripts/Game.cs
+++ SuperMario/Assets/Scripts/Game.cs
@@ -9,10 +9,11 @@ public class Game : MonoBehaviour
     [SerializeField] TextMeshProUGUI Score;
     [SerializeField] TextMeshProUGUI Coins;
     [SerializeField] TextMeshProUGUI Time;
+    public GameObject stone;
 
 
-    int playerScore = 5;
-    int playerCoins = 5;
+    int playerScore = 0;
+    int playerCoins = 0;
     int initialTime = 400;
     DateTime time1;
     TimeSpan interval;
@@ -25,11 +26,40 @@ public class Game : MonoBehaviour
     }
     void Update()
     {
+        //timer
         DateTime time2 = DateTime.Now;
         interval = time2 - time1;
         int time = initialTime - interval.Seconds;
+        //UI
         Time.text = "TIME\n" + time.ToString().PadLeft(3, '0');
+        Coins.text = "x" + playerCoins.ToString().PadLeft(2, '0');
+        Score.text = "MARIO\n" + playerScore.ToString().PadLeft(6, '0');
 
+        //detect what the mouse clicks on
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject.name == "Question(Clone)")
+                {
+                    playerCoins++;
+                    playerScore += 200;
+
+                    GameObject ToSpawn = stone;
+                    ToSpawn.transform.position = hit.transform.position;
+                    Destroy(hit.transform.gameObject);
+                    GameObject.Instantiate(ToSpawn);
+                }
+                else if (hit.transform.gameObject.name == "Brick(Clone)")
+                {
+                    Destroy(hit.transform.gameObject);
+                }
+            }
+
+        }
 
     }
+
 }
